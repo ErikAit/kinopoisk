e4a269204571ea47f3be1e1bc337baa689d79cc6
Erik <eait84344@gmail.com>
2024-06-07 16:05:31 +0400

    category.jsx is removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 // components import
-import Category from '../Category/Category'
 
 export default function Home() {
+
+
   return (
-    <div className='Home'>
-      <div className="Home__content">
-        <Category />
+    <div className='Home absolute top-[95vh]'>
+      <div className="Home__content px-[70px]">
+        <h2 className='text-white text-[32px] font-[700]'>Популярные</h2>
+        <div className="category__container">
+
+        </div>
       </div>
     </div>
   )
