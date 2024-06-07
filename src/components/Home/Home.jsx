12630518af5ea9199f6d7220964d84e25d6de106
Erik <eait84344@gmail.com>
2024-06-07 15:45:32 +0400

    added new component Category.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Category from '../Category/Category'
 
 export default function Home() {
   return (
     <div className='Home'>
-
+      <div className="Home__content">
+        <Category />
+      </div>
     </div>
   )
 }
