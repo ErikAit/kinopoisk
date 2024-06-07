9276328aaf5309397396c0e36c0300a7e9fb3a18
Erik <eait84344@gmail.com>
2024-06-07 15:44:20 +0400

    added new component Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 
 // component imports
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+
+      <Home />
     </div>
   )
 }
